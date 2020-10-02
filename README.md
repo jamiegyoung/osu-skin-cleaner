9d67a0cb474835b28cb507bf23f6ce658d297b05
jamiegyoung <jamiegyoung22@gmail.com>
2020-10-02 05:39:40 +0100

    Added parameters - Quiet mode added - Auto copy and osk params added - Parameter documentation added

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # osu-skin-cleaner
+
 A simple script to copy osu! skins to the current directory, remove the psd files, and then put them in an osk format
 
-## Usage
-skincleaner.ps1 -dir "Dir containing osu skins"
+## Example Usage
+
+`skincleaner.ps1 -dir "**Dir containing osu skins**"`
+
+`skincleaner.ps1 -q -osk`
+
+`skincleaner.ps1 -q -copy -dir "**Dir containing osu skins**" `
+
+## Parameters
+
+_All parameters are optional_
+|Name|Description|Notes|
+|----|-----------|-----|
+|-dir |the directory to copy the skins from|requires a path to be passed `-dir 'C:\Skins'`|
+|-copy|copies the folders||
+|-osk |makes osks from folders in the current directory||
+|-q |quiet mode, removes all prompts|`-dir` is required if `-q` and `-copy` are used|
+
+Note: _if `-copy` or `-osk` is not paired with `-q` the user will be prompted for the other option._
--- skincleaner.ps1
+++ skincleaner.ps1
@@ -1,12 +1,55 @@
-param ($dir)
 
-Copy-Item -Path "$dir\*" -Destination "./" -Recurse
-Get-ChildItem "*.psd" -Recurse | ForEach-Object {
-  Remove-Item -Path "$_"
+param($dir)
+
+function Convert-Osk {
+  Get-ChildItem -Directory | Foreach-Object {
+    $skinpath = $_.FullName
+    Compress-Archive -Path "$skinpath\*" -DestinationPath "$skinpath.zip"
+    Rename-Item -Path "$skinpath.zip" -NewName "$skinpath.osk"
+  }
 }
 
-Get-ChildItem -Directory | Foreach-Object {
-  $skinpath = $_.FullName
-  Compress-Archive -Path "$skinpath\*" -DestinationPath "$skinpath.zip"
-  Rename-Item -Path "$skinpath.zip" -NewName "$skinpath.osk"
+function Copy-Skins {
+  param(
+    $quiet
+  )
+  if ($null -eq $dir) {
+    if ($quiet) {
+      Write-Error 'Path not passed: hint: -dir "path"'
+      Return
+    }
+    else {
+      $dir = Read-Host 'Enter the path you wish to copy'
+    }
+  }
+  if (Test-Path $dir) {
+    Copy-Item -Path "$dir\*" -Destination "./" -Recurse
+    Get-ChildItem "*.psd" -Recurse | ForEach-Object {
+      Remove-Item -Path "$_"
+    }
+  }
+  else {
+    Write-Error 'Path does not exist'
+  }
+}
+
+$quiet = $args.Contains('-q')
+if ($args -contains '-copy') {
+  Copy-Skins $quiet
+}
+elseif (-not $quiet) {
+  $copyanswer = Read-Host 'Copy the skin folders? [Y/N]'
+  if ($copyanswer.ToLower() -eq 'y') {
+    Copy-Skins $dir
+  } 
+}
+
+if ($args -contains '-osk') {
+  Convert-Osk
+}
+elseif (-not $quiet) {
+  $oskanswer = Read-Host 'Make osk files out of folders in the current directory? [Y/N]'
+  if ($oskanswer.ToLower() -eq 'y') {
+    Convert-Osk
+  }
 }
