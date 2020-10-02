ecef7b0f2ce0df1406dadd80d08627ecda748f29
jamiegyoung <jamiegyoung22@gmail.com>
2020-10-02 16:40:43 +0100

    Added loading bar to removing osk files

--- skincleaner.ps1
+++ skincleaner.ps1
@@ -2,9 +2,9 @@ param($dir)
 
 function Convert-Osk {
   Get-ChildItem -Directory | Foreach-Object {
-    $skinpath = $_.FullName
-    Compress-Archive -Path "$skinpath\*" -DestinationPath "$skinpath.zip"
-    Rename-Item -Path "$skinpath.zip" -NewName "$skinpath.osk"
+    $skinPath = $_.FullName
+    Compress-Archive -Path "$skinPath\*" -DestinationPath "$skinPath.zip"
+    Rename-Item -Path "$skinPath.zip" -NewName "$skinPath.osk"
   }
 }
 
@@ -22,17 +22,18 @@ function Copy-Skins {
     }
   }
   if (Test-Path $dir) {
-    $filecount = (Get-ChildItem "$dir" -Directory | Measure-Object).count
+    $fileCount = (Get-ChildItem "$dir" -Directory | Measure-Object).count
     $counter = 0
-    Write-Progress -Activity "Copying Folders" -Status "$p% Complete: " -PercentComplete ($counter / $filecount * 100)
+    Write-Progress -Activity "Copying Folders" -Status "$p% Complete: " -PercentComplete ($counter / $fileCount * 100)
     Get-ChildItem "$dir" -Directory | Foreach-Object {
       $fullpath = $_.FullName
       if (!(Test-Path -path "./$_/")) {New-Item "./$_/" -Type Directory | Out-Null}
       Copy-Item -Path "$fullpath\*" -Destination "./$_/" -Recurse
       $counter++
-      $percent = [math]::Ceiling($counter / $filecount * 100)
+      $percent = [math]::Ceiling($counter / $fileCount * 100)
       Write-Progress -Activity "Copying Folders" -Status "$percent% Complete: " -PercentComplete $percent
     }
+    Write-Progress -Activity "Copying Folders" -Completed
   }
   else {
     Write-Error 'Path does not exist'
@@ -40,9 +41,15 @@ function Copy-Skins {
 }
 
 function Remove-Psd {
+  $totalPsds = (Get-ChildItem "*.psd" -Recurse | Measure-Object).Count
+  $counter = 0
   Get-ChildItem "*.psd" -Recurse | ForEach-Object {
-    Remove-Item -Path "$_"
+    Remove-Item -Path $_.FullName
+    $counter++
+    $percent = [math]::Ceiling($counter / $totalPsds * 100)
+    Write-Progress -Activity "Deleting PSD files" -Status "$percent% Complete: " -PercentComplete $percent
   }
+  Write-Progress -Activity "Deleting PSD files" -Completed
 }
 
 function Main {
@@ -54,8 +61,8 @@ function Main {
     Copy-Skins $quiet
   }
   elseif (-not $quiet) {
-    $copyanswer = Read-Host 'Copy the skin folders? [Y/N]'
-    if ($copyanswer.ToLower() -eq 'y') {
+    $copyAnswer = Read-Host 'Copy the skin folders? [Y/N]'
+    if ($copyAnswer.ToLower() -eq 'y') {
       Copy-Skins $dir
     } 
   }
@@ -63,8 +70,8 @@ function Main {
   if ($programArgs -contains '-rpsd') {
     Remove-Psd
   } elseif (-not $quiet) {
-    $psdanswer = Read-Host 'Remove all psd files from all folders in the current directory and any folders inside? [Y/N]'
-    if ($psdanswer.ToLower() -eq 'y') {
+    $psdAnswer = Read-Host 'Remove all psd files from all folders in the current directory and any folders inside? [Y/N]'
+    if ($psdAnswer.ToLower() -eq 'y') {
       Remove-Psd
     }
   }
@@ -73,8 +80,8 @@ function Main {
     Convert-Osk
   }
   elseif (-not $quiet) {
-    $oskanswer = Read-Host 'Make osk files out of folders in the current directory? [Y/N]'
-    if ($oskanswer.ToLower() -eq 'y') {
+    $oskAnswer = Read-Host 'Make osk files out of folders in the current directory? [Y/N]'
+    if ($oskAnswer.ToLower() -eq 'y') {
       Convert-Osk
     }
   }
